a3de0eece9d79f1ff1b1b79303a4046a6fdb7023
minhpg <67231681+minhpg@users.noreply.github.com>
2022-10-28 16:37:47 +1100

    fix db connection

--- settings.php
+++ settings.php
@@ -4,8 +4,8 @@ $THEME = 'light';
 $SESSION_NAME = "SESSION_NAME";
 
 // MySQL Connection
-// $MYSQL_HOST = "feenix-mariadb.swin.edu.au";
-$MYSQL_HOST = "localhost";
+$MYSQL_HOST = "feenix-mariadb.swin.edu.au";
+// $MYSQL_HOST = "localhost";
 $MYSQL_USER = "s103808249";
 $MYSQL_PWD = "040804";
 $MYSQL_DB = "s103808249_db";
