a346dac3ac8bea4062eb4ae0c5985e592f1b3f1e
minhpg <67231681+minhpg@users.noreply.github.com>
2022-10-28 22:37:54 +1100

    api fix

--- manage/api/orders/delete_order.php
+++ manage/api/orders/delete_order.php
@@ -1,8 +1,8 @@
 <?php 
-include('import.php');
+include('imports.php');
 
-if(!$_POST) header('location: ../../index.php');
-$id = $_POST['order_id'];
+if(!$_GET) header('location: ../../index.php');
+$id = $_GET['order_id'];
 $order_schema = new Order();
 $order_schema->deleteById($id);
 header('location: '.$_SERVER['HTTP_REFERER']);
